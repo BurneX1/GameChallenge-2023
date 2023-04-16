fc24ed4c61d31b94d828fda5d96e8e03724306ea
Alejandro Medina <almpratto@gmail.com>
2023-04-16 17:01:10 -0500

    repush de menu

---
+++ Assets/SwipeController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class SwipeController : MonoBehaviour
+{
+    //public UnityEvent LeftFunc = delegate { };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwipeLeft();
+        SwipeRigth();
+        SwipeUp();
+        SwipeDown();
+    }
+
+    void SwipeLeft()
+    {
+
+    }
+
+    void SwipeRigth()
+    {
+
+    }
+
+    void SwipeUp()
+    {
+
+    }
+
+    void SwipeDown()
+    {
+
+    }
+}
+
